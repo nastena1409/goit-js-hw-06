361fac9254618fed600503a49af3110e40018f47
nastena1409 <nastena1409@gmail.com>
2023-04-12 16:54:26 +0200

    Create task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#name-input');
+const outputEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+
+    if (inputEl.textContent = "")
+        outputEl.textContent = "Anonymous";
+    else {
+        outputEl.textContent = event.currentTarget.value;
+    }
+    
+}
+
