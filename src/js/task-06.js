7c66431a5b6f44549d04afae3540a9425aaf2125
nastena1409 <nastena1409@gmail.com>
2023-04-13 11:59:07 +0200

    update task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,21 @@
+const inputEl = document.querySelector('#validation-input');
+const length = Number(inputEl.getAttribute('data-length'));
+console.log(typeof length);
+
+
+inputEl.addEventListener("blur", onInputBlur);
+
+function onInputBlur(event) {
+    if (event.currentTarget.value.length === length) {
+        inputEl.classList.add("valid");  
+        inputEl.classList.remove("invalid");   
+    }
+    
+    else {
+        inputEl.classList.add("invalid");
+        inputEl.classList.remove("valid");
+    }
+};
+
+
+
