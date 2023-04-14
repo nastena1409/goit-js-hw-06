0ef3eb07db46b96aefd17e59417ca3b27f9adbf7
nastena1409 <nastena1409@gmail.com>
2023-04-14 14:01:52 +0200

    create task 8

---
+++ src/js/task-07.js
@@ -0,0 +1,16 @@
+const inputEl = document.querySelector("#font-size-control");
+const textEl = document.querySelector("#text");
+
+
+
+
+
+inputEl.addEventListener("input", onInputChange);
+
+    function onInputChange(event) {
+        textEl.style.fontSize = `${event.currentTarget.value}px`;
+};
+
+
+
+
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const formEl = document.querySelector(".login-form");
+//console.log(formEl.elements);
 
+formEl.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    
+    const 
+        {
+            elements: { email, password }
+        } = event.currentTarget;
+    
+    if (!email.value || !password.value) {
+        alert('Please enter your email and password!')
+    } else {
+        const userData = {
+            email: email.value,
+            password: password.value,
+        }
+
+        console.dir(userData);
+        
+    }
+    event.currentTarget.reset();
+})
