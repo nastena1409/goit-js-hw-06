1fb9c2526a567a350fa86c3a4d26ad1a0f1c00b1
nastena1409 <nastena1409@gmail.com>
2023-04-14 15:06:41 +0200

    create task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const refs = {
+  body: document.querySelector('body'),
+  changeColorBtn: document.querySelector('.change-color'),
+  colorValue: document.querySelector('.color'),
+}
+
+refs.changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorValue.textContent = getRandomHexColor();
+}
+  
